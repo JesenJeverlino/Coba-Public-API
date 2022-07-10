7bfe50a6115fa2a3ebc9b4b4760a4b599de60735
JesenJeverlino <jesenjeverlino22@gmail.com>
2022-07-10 21:21:43 +0700

    latihan public api

---
+++ script.js
@@ -0,0 +1,123 @@
+$("#search-button").on("click", function () {
+  searchMovie();
+});
+
+$("#search-input").on("keyup", function (e) {
+  if (e.keyCode === 13) {
+    searchMovie();
+  }
+});
+
+$("#movie-list").on("click", ".see-detail", function () {
+  $.ajax({
+    url: "http://www.omdbapi.com",
+    type: "get",
+    data: {
+      apikey: "c63b4aa4",
+      i: $(this).data("id"),
+    },
+    success: function (movie) {
+      if (movie.Response === "True") {
+        $(".modal-body").html(
+          `
+        <div class="fluid-container">
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="` +
+            movie.Poster +
+            `" class="img-fluid"/>
+                </div>
+                
+                <div class="col-md-8">
+                    <ul class="list-group">
+                         <li class="list-group-item"><h3>` +
+            movie.Title +
+            `</h3></li>
+                        <li class="list-group-item">Released : ` +
+            movie.Released +
+            `</li>
+                        <li class="list-group-item">Genre : ` +
+            movie.Genre +
+            `</li>
+                        <li class="list-group-item">Director : ` +
+            movie.Director +
+            `</li>
+                        <li class="list-group-item">Actors : ` +
+            movie.Actors +
+            `</li>
+                        <li class="list-group-item">Writer : ` +
+            movie.Writer +
+            `</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        `
+        );
+      }
+    },
+  });
+});
+
+function searchMovie() {
+  $("#movie-list").html("");
+  $.ajax({
+    url: "http://www.omdbapi.com",
+    type: "get",
+    dataType: "json",
+    data: {
+      apikey: "c63b4aa4",
+      s: $("#search-input").val(),
+    },
+    success: function (result) {
+      if (result.Response == "True") {
+        let movies = result.Search;
+        $.each(movies, function (i, data) {
+          $("#movie-list").append(
+            `
+              <div class="col-md-4">
+              <div class="card">
+              <img src="` +
+              data.Poster +
+              `" class="card-img-top" alt="...">
+              <div class="card-body">
+              <h5 class="card-title">` +
+              data.Title +
+              `</h5>
+              <h6 class="card-subtitle mb-2 text-muted">` +
+              data.Year +
+              `</h6>
+                <a href="#" class="card-link see-detail" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="` +
+              data.imdbID +
+              `">See Details</a>
+              </div>
+              </div>
+              </div>
+              `
+          );
+        });
+
+        $("#search-input").val("");
+      } else {
+        $("#movie-list").html(
+          `
+          <div class="col">
+          <h1 class="text-center">` +
+            result.Error +
+            `</h1>
+          </div>
+          `
+        );
+      }
+    },
+  });
+}
+
+$.ajax({
+  url: "https://api.spaceflightnewsapi.net/v3",
+  type: "get",
+  dataType: "json",
+  success: function (e) {
+    console.log(e);
+  },
+});
